f30ffe8decdc780c5806a5a885826c2aa1a4debe
Alelsito <lbalelxander@gmail.com>
2022-11-06 19:05:38 -0600

    feat: Kata Genetic Algorithm Series #3 Crossover

--- index.js
+++ index.js
@@ -71,37 +71,52 @@
 
 /* Genetic Algorithm Series - #2 Mutation */
 
-const mutate = (chromosome, p) => {
-  let mutate = []
-  const split = chromosome.split('')
+// const mutate = (chromosome, p) => {
+//   let mutate = []
+//   const split = chromosome.split('')
 
-  if (p === 1) {
-    split.forEach(e => e === '1' ? mutate.push(0) : mutate.push(1))
-  } else if (p === 0) {
-    mutate = split
-  } else {
-    split.forEach(e => {
-      const r = Math.random()
-      if (r < p) {
-        if (e === '1') { mutate.push(0) } else { mutate.push(1) }
-      } else (mutate.push(parseInt(e)))
-    })
-  }
+//   if (p === 1) {
+//     split.forEach(e => e === '1' ? mutate.push(0) : mutate.push(1))
+//   } else if (p === 0) {
+//     mutate = split
+//   } else {
+//     split.forEach(e => {
+//       const r = Math.random()
+//       if (r < p) {
+//         if (e === '1') { mutate.push(0) } else { mutate.push(1) }
+//       } else (mutate.push(parseInt(e)))
+//     })
+//   }
 
-  return mutate.join('')
+//   return mutate.join('')
+// }
+
+// const zero = Array(10).join('0')
+// const one = Array(10).join('1')
+
+// // 100% mutate
+// console.log(mutate(zero, 1))
+// console.log(mutate(one, 1))
+
+// // 0% mutate
+// console.log(mutate(zero, 0))
+// console.log(mutate(one, 0))
+
+// // 50% mutate
+// console.log(mutate(zero, 0.5))
+// console.log(mutate(one, 0.5))
+
+/* Genetic Algorithm Series - #3 Crossover */
+
+const crossover = (chromosome1, chromosome2, index) => {
+  const baseCrossoverChromosome1 = chromosome1.substring(0, index).split('')
+  const baseCrossoverChromosome2 = chromosome2.substring(0, index).split('')
+
+  chromosome1.substring(index).split('').forEach(e => baseCrossoverChromosome2.push(e))
+  chromosome2.substring(index).split('').forEach(e => baseCrossoverChromosome1.push(e))
+
+  return [baseCrossoverChromosome1.join(''), baseCrossoverChromosome2.join('')]
 }
 
-const zero = Array(10).join('0')
-const one = Array(10).join('1')
-
-// 100% mutate
-console.log(mutate(zero, 1))
-console.log(mutate(one, 1))
-
-// 0% mutate
-console.log(mutate(zero, 0))
-console.log(mutate(one, 0))
-
-// 50% mutate
-console.log(mutate(zero, 0.5))
-console.log(mutate(one, 0.5))
+console.log(crossover('111000', '000110', 3))
+console.log(crossover('110', '001', 2))
